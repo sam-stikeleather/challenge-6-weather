ccac1f74b6f9b8aa3d31c2379dc7ff191be96c1a
Sam <sam.stikeleather@gmail.com>
2023-07-31 12:56:11 -0400

    set up variables in JS

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+const API_KEY = '801ac2cdb3eabc4ac8b4a3e66aabd2e7';
+const searchForm = document.querySelector('searchForm');
+const cityInput = document.querySelector('cityInput');
+const currentWeatherDiv = document.querySelector('currentWeatherDiv');
+const forecastDiv = document.querySelector('forecastDiv');
+const historyList = document.querySelector('historyList');
+
+
+
