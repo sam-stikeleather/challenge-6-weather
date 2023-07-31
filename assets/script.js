c93c39281c6e21075400ea4b985f8fffb7f0ff97
Sam <sam.stikeleather@gmail.com>
2023-07-31 12:59:04 -0400

    set up fetch request to weather api

--- assets/script.js
+++ assets/script.js
@@ -5,5 +5,14 @@ const currentWeatherDiv = document.querySelector('currentWeatherDiv');
 const forecastDiv = document.querySelector('forecastDiv');
 const historyList = document.querySelector('historyList');
 
+let searchHistory = [];
 
+function fetchWeatherData(city) {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=imperial`;
+
+    fetch(apiUrl)
+        .then((response) => response.json())
+        .then(data => displayWeatherData(data))
+        .catch(error => console.log('Error fetching data:', error));
+}
 
